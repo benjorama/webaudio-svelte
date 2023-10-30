a8330c791fefba43630d1243b0b6f0656b6b8098
benjorama <benmacomb@gmail.com>
2023-10-29 18:50:54 -0600

    Adds compressor functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -35,6 +35,9 @@
 	/** @type {BiquadFilterNode} */
 	let biquadFilterNode;
 
+	/** @type{DynamicsCompressorNode}*/
+	let dynamicCompressorNode;
+
 	/** @type {boolean} */
 	let makingNoise;
 
@@ -72,11 +75,13 @@
 			gainNode = audioContext.createGain();
 			stereoPannerNode = audioContext.createStereoPanner();
 			biquadFilterNode = audioContext.createBiquadFilter();
+			dynamicCompressorNode = audioContext.createDynamicsCompressor();
 
 			oscillatorNode.connect(gainNode);
 			gainNode.connect(stereoPannerNode);
 			stereoPannerNode.connect(biquadFilterNode);
-			biquadFilterNode.connect(audioContext.destination);
+			biquadFilterNode.connect(dynamicCompressorNode);
+			dynamicCompressorNode.connect(audioContext.destination);
 
 			gainNode.gain.value = 0.1;
 			stereoPannerNode.pan.value = 0.0;
@@ -129,6 +134,61 @@
 	function selectFilter(event) {
 		let filterInput = /**@type{HTMLSelectElement}*/ (event.currentTarget);
 		biquadFilterNode.type = /** @type {BiquadFilterType} */ (filterInput.value);
+	}
+
+	/**
+	 *@param {Event} event
+	 */
+	function adjustCompressorThreshold(event) {
+		let thresholdInput = /**@type {HTMLInputElement}*/ (event.currentTarget);
+		dynamicCompressorNode.threshold.setValueAtTime(
+			parseFloat(thresholdInput.value),
+			audioContextCurrentTime + 0.01
+		);
+	}
+
+	/**
+	 *@param {Event} event
+	 */
+	function adjustCompressorAttack(event) {
+		let thresholdInput = /**@type {HTMLInputElement}*/ (event.currentTarget);
+		dynamicCompressorNode.attack.setValueAtTime(
+			parseFloat(thresholdInput.value),
+			audioContextCurrentTime + 0.01
+		);
+	}
+
+	/**
+	 *@param {Event} event
+	 */
+	function adjustCompressorKnee(event) {
+		let kneeInput = /**@type {HTMLInputElement}*/ (event.currentTarget);
+		dynamicCompressorNode.knee.setValueAtTime(
+			parseFloat(kneeInput.value),
+			audioContextCurrentTime + 0.01
+		);
+	}
+
+	/**
+	 *@param {Event} event
+	 */
+	function adjustCompressorRatio(event) {
+		let ratioInput = /**@type {HTMLInputElement}*/ (event.currentTarget);
+		dynamicCompressorNode.ratio.setValueAtTime(
+			parseFloat(ratioInput.value),
+			audioContextCurrentTime + 0.01
+		);
+	}
+
+	/**
+	 *@param {Event} event
+	 */
+	function adjustCompressorRelease(event) {
+		let releaseInput = /**@type {HTMLInputElement}*/ (event.currentTarget);
+		dynamicCompressorNode.release.setValueAtTime(
+			parseFloat(releaseInput.value),
+			audioContextCurrentTime + 0.01
+		);
 	}
 </script>
 
@@ -194,6 +254,78 @@
 					<option value="notch">notch</option>
 					<option value="allpass">allpass</option>
 				</select>
+			</div>
+			<div>
+				<p>Compressor</p>
+				<div>
+					<input
+						on:input={adjustCompressorThreshold}
+						type="range"
+						id="compressor-threshold"
+						name="compressor-threshold"
+						min="-100"
+						max="0"
+						step="1.0"
+						value="-24"
+					/>
+					<label for="compressor-threshold">Threshold</label>
+				</div>
+
+				<div>
+					<input
+						on:input={adjustCompressorAttack}
+						type="range"
+						id="compressor-attack"
+						name="compressor-attack"
+						min="0"
+						max="1"
+						step="0.001"
+						value="0.003"
+					/>
+					<label for="compressor-attack">Attack</label>
+				</div>
+
+				<div>
+					<input
+						on:input={adjustCompressorKnee}
+						type="range"
+						id="compressor-knee"
+						name="compressor-knee"
+						min="0"
+						max="40"
+						step="1.0"
+						value="30"
+					/>
+					<label for="compressor-knee">Knee</label>
+				</div>
+
+				<div>
+					<input
+						on:input={adjustCompressorRatio}
+						type="range"
+						id="compressor-ratio"
+						name="compressor-ratio"
+						min="1.0"
+						max="20"
+						step="1.0"
+						value="12"
+					/>
+					<label for="compressor-ratio">Ratio</label>
+				</div>
+
+				<div>
+					<input
+						on:input={adjustCompressorRelease}
+						type="range"
+						id="compressor-release"
+						name="compressor-release"
+						min="0.0"
+						max="1.0"
+						step="0.1"
+						value="0.25"
+					/>
+					<label for="compressor-release">Release</label>
+				</div>
 			</div>
 			<p>type: {oscillatorNode.type}</p>
 			<p>frequency value: {oscillatorNode.frequency.value}</p>
